ef0b9f3def28421c0ed46a2e67f516e76ec4f95e
khaledtf19 <khaledtf19@gmail.com>
2022-04-21 19:03:34 +0200

    update

--- src/route/Checkout/Checkout.component.js
+++ src/route/Checkout/Checkout.component.js
@@ -10,23 +10,41 @@ class Checkout extends SourceCheckout {
   renderStepsBar = () => {
     const selector = document.querySelector(".Checkout");
 
+    const steps = Object.keys(this.stepMap);
+
     let bar = document.createElement("div");
     bar.classList.add("Checkout__bar__container");
 
-    let progressLine = document.createElement("div");
-    progressLine.classList.add("Checkout__bar__progressLine");
+    let progressLine1 = document.createElement("div");
+    progressLine1.classList.add("Checkout__bar__progressLine", "progressLine1");
 
     let firstPoint = document.createElement("div");
     firstPoint.classList.add("Checkout__bar__firstPoint");
 
+    let progressLine2 = document.createElement("div");
+    progressLine2.classList.add("Checkout__bar__progressLine", "progressLine2");
+
     let secondPoint = document.createElement("div");
     secondPoint.classList.add("Checkout__bar__secondPoint");
 
-    bar.appendChild(progressLine);
+    let progressLine3 = document.createElement("div");
+    progressLine3.classList.add("Checkout__bar__progressLine", "progressLine3");
+
+    let firstPontP = document.createElement("p");
+    firstPontP.append("firstPoint");
+    firstPoint.appendChild(firstPontP);
+
+    let secondPointP = document.createElement("p");
+    secondPointP.append("secondPoint");
+    secondPoint.appendChild(secondPointP);
+
+    bar.appendChild(progressLine1);
+    bar.appendChild(progressLine2);
+    bar.appendChild(progressLine3);
+
     bar.appendChild(firstPoint);
     bar.appendChild(secondPoint);
 
-    console.log(selector);
     selector.appendChild(bar);
     return selector;
   };
@@ -35,6 +53,11 @@ class Checkout extends SourceCheckout {
     this.renderStepsBar();
     console.log(this.props.checkoutStep);
     console.log(this.stepMap);
+  }
+
+  componentDidUpdate(prevProp) {
+    if (prevProp.checkoutStep !== this.props.checkoutStep) {
+    }
   }
 }
 
