526e31f1f4f3eab0fb032a72544ecaeb5bd6fb1c
khaledtf19 <khaledtf19@gmail.com>
2022-04-22 17:45:55 +0200

    fix

--- src/route/Checkout/Checkout.component.js
+++ src/route/Checkout/Checkout.component.js
@@ -1,4 +1,5 @@
 import { Checkout as SourceCheckout } from "SourceRoute/Checkout/Checkout.component";
+import { ContentWrapper as SourceContentWrapper } from "SourceComponent/ContentWrapper/ContentWrapper.component";
 
 import "./Checkout.extension.style.scss";
 
@@ -8,45 +9,11 @@ class Checkout extends SourceCheckout {
   }
 
   renderStepsBar = () => {
-    const selector = document.querySelector(".Checkout");
-
-    const steps = Object.keys(this.stepMap);
-
-    let bar = document.createElement("div");
-    bar.classList.add("Checkout__bar__container");
-
-    let progressLine1 = document.createElement("div");
-    progressLine1.classList.add("Checkout__bar__progressLine", "progressLine1");
-
-    let firstPoint = document.createElement("div");
-    firstPoint.classList.add("Checkout__bar__firstPoint");
-
-    let progressLine2 = document.createElement("div");
-    progressLine2.classList.add("Checkout__bar__progressLine", "progressLine2");
-
-    let secondPoint = document.createElement("div");
-    secondPoint.classList.add("Checkout__bar__secondPoint");
-
-    let progressLine3 = document.createElement("div");
-    progressLine3.classList.add("Checkout__bar__progressLine", "progressLine3");
-
-    let firstPontP = document.createElement("p");
-    firstPontP.append("firstPoint");
-    firstPoint.appendChild(firstPontP);
-
-    let secondPointP = document.createElement("p");
-    secondPointP.append("secondPoint");
-    secondPoint.appendChild(secondPointP);
-
-    bar.appendChild(progressLine1);
-    bar.appendChild(progressLine2);
-    bar.appendChild(progressLine3);
-
-    bar.appendChild(firstPoint);
-    bar.appendChild(secondPoint);
-
-    selector.appendChild(bar);
-    return selector;
+    return (
+      <div>
+        <h1>hi</h1>
+      </div>
+    );
   };
 
   componentDidMount() {
@@ -58,6 +25,31 @@ class Checkout extends SourceCheckout {
   componentDidUpdate(prevProp) {
     if (prevProp.checkoutStep !== this.props.checkoutStep) {
     }
+  }
+
+  render() {
+    return (
+      <main block="Checkout">
+        {this.renderStepsBar()}
+        <SourceContentWrapper
+          wrapperMix={{ block: "Checkout", elem: "Wrapper" }}
+          label={__("Checkout page")}
+        >
+          {this.renderSummary(true)}
+          <div block="Checkout" elem="Step">
+            {this.renderTitle()}
+            {this.renderGuestForm()}
+            {this.renderStep()}
+            {this.renderLoader()}
+          </div>
+          <div>
+            {this.renderSummary()}
+            {this.renderPromo()}
+            {this.renderCoupon()}
+          </div>
+        </SourceContentWrapper>
+      </main>
+    );
   }
 }
 
